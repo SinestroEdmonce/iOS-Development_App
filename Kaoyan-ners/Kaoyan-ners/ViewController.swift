aee1697cfda01cec046a9793a75ee3c808c92a21
SinestroEdmonce <867208887@qq.com>
2018-11-30 15:55:25 +0800

    Construct the project again and start implementing

---
+++ Kaoyan-ners/Kaoyan-ners/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kaoyan-ners
+//
+//  Created by mac on 2018/11/30.
+//  Copyright © 2018年 cn.nju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Kaoyan-ners/Kaoyan-nersTests/Kaoyan_nersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kaoyan_nersTests.swift
+//  Kaoyan-nersTests
+//
+//  Created by mac on 2018/11/30.
+//  Copyright © 2018年 cn.nju. All rights reserved.
+//
+
+import XCTest
+@testable import Kaoyan_ners
+
+class Kaoyan_nersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
