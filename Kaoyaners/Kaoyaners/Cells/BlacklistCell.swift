0e60ce7b5f5ec165321120bfabfd55edb4c138c1
SinestroEdmonce <867208887@qq.com>
2019-01-01 16:23:26 +0800

    Update: 2. Update UI controller

---
+++ Kaoyaners/Kaoyaners/Cells/BlacklistCell.swift
@@ -0,0 +1,24 @@
+//
+//  BlacklistCell.swift
+//  Kaoyaners
+//
+//  Created by sinestro on 2019/1/1.
+//  Copyright © 2019 cn.nju. All rights reserved.
+//
+
+import UIKit
+
+class BlacklistCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Kaoyaners/Kaoyaners/Cells/HomeRecommendCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeRecommendCell.swift
+//  Kaoyaners
+//
+//  Created by sinestro on 2019/1/1.
+//  Copyright © 2019 cn.nju. All rights reserved.
+//
+
+import UIKit
+
+class HomeRecommendCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
