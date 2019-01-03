bb48fc1dc73d897b7ff0f04a4aa3d0c1c1467bf7
SinestroEdmonce <867208887@qq.com>
2019-01-03 19:56:17 +0800

    Update: 6. Update home UI -> PAGE VIEW

---
+++ Kaoyaners/Kaoyaners/ViewControllers/HomeController/FavouriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavouriteViewController.swift
+//  Kaoyaners
+//
+//  Created by sinestro on 2019/1/3.
+//  Copyright © 2019 cn.nju. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Kaoyaners/Kaoyaners/ViewControllers/HomeController/HomeViewController.swift
@@ -0,0 +1,114 @@
+//
+//  HomeViewController.swift
+//  Kaoyaners
+//
+//  Created by sinestro on 2019/1/3.
+//  Copyright © 2019 cn.nju. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // Variable used to describe UIPageVC
+    var pageVC: UIPageViewController!
+    
+    // Four categories of resource
+    var favouriteVC: FavouriteViewController!
+    var recommendVC: RecommendViewController!
+    // View data array used to store the four view controllers
+    var contentController = [UIViewController]()
+    
+    // Slider view used as a reminder to reflect actions
+    
+    var sliderImageView: UIImageView!
+    
+    // Variables used to help change the pages
+    var lastPage = 0
+    var currentPage: Int = 0 {
+        // Attributes observation
+        didSet {
+            // According to the current page, obtain the offset
+            // A tiny motion animation
+            let pageOffset = (self.view.frame.width/4.0)*CGFloat(self.currentPage)
+            UIView.animate(withDuration: 0.2) { () -> Void in
+                self.sliderImageView.frame.origin = CGPoint(x: pageOffset, y: -1)
+            }
+            
+            // According to the relationship between the current page and last page, change the page view
+            if currentPage > lastPage {
+                self.pageVC.setViewControllers([self.contentController[self.currentPage]], direction: .forward, animated: true, completion: nil)
+            }
+            else {
+                self.pageVC.setViewControllers([self.contentController[self.currentPage]], direction: .reverse, animated: true, completion: nil)
+            }
+            
+            self.lastPage = self.currentPage
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Obtain the page view controller
+        for childVC in self.children {
+            if childVC.isKind(of: UIPageViewController.self){
+                self.pageVC = childVC as? UIPageViewController
+                break
+            }
+        }
+        
+        // According to Storyboard ID to initialize the variables
+        self.favouriteVC = storyboard?.instantiateViewController(withIdentifier: "FavouriteContentVCID") as? FavouriteViewController
+        self.recommendVC = storyboard?.instantiateViewController(withIdentifier: "RecommendVCID") as? RecommendViewController
+        
+        // Set data source delegate of pageViewController the current controller
+        self.pageVC.dataSource = self
+        // Manually provide a page for pageViewController
+        self.pageVC.setViewControllers([self.recommendVC], direction: UIPageViewController.NavigationDirection.forward, animated: true, completion: nil)
+        
+        // Add views into the view data array
+        self.contentController.append(self.recommendVC)
+        self.contentController.append(self.favouriteVC)
+        
+        // Add slider image
+        self.sliderImageView = UIImageView(frame: CGRect(x: 0, y: -1, width: self.view.frame.width / 4.0, height: 3.0))
+        self.sliderImageView.image = UIImage(named: "AvatarBackground")
+        //self.sliderView.addSubview(sliderImageView)
+        
+        // Accept the notification to tell whether the page been changed
+        NotificationCenter.default.addObserver(self, selector: #selector(currentPageChanged(notification:)), name: NSNotification.Name(rawValue: "currentPageChanged"), object: nil)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    // Methods to response the notification
+    @objc func currentPageChanged(notification: NSNotification) {
+        self.currentPage = notification.object as! Int
+    }
+    
+    // Change the current page to another one
+    @IBAction func changeCurrentPage(_ sender: Any) {
+        self.currentPage = (sender as! UIButton).tag - 200
+    }
+    
+}
+
+extension HomeViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        if viewController.isKind(of: RecommendViewController.self){
+            return self.favouriteVC
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        if viewController.isKind(of: FavouriteViewController.self){
+            return self.recommendVC
+        }
+        return nil
+    }
+    
+}
+
